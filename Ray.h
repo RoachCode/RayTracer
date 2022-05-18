32a898eccece2ecdec157b9a4a2da069a5413b28
RoachCode <74122730+RoachCode@users.noreply.github.com>
2022-05-18 15:48:56 -0700

    Create Ray.h

---
+++ Ray.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Vec3.h"
+
+class ray 
+{
+public:
+    point3 orig;
+    Vec3 dir;
+    ray() {}
+    ray(const point3& origin, const Vec3& direction)
+        : orig(origin), dir(direction)
+    {}
+
+    point3 origin() const { return orig; }
+    Vec3 direction() const { return dir; }
+
+    // Linear interpolation 
+    point3 at(double t) const
+    {
+        return orig + t * dir;
+    }
+
+
+};
