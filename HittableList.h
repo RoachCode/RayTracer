341b4ef115142b7e3fa5a37fb7b11421a900962c
RoachCode <74122730+RoachCode@users.noreply.github.com>
2022-05-18 15:50:14 -0700

    Create HittableList.h

---
+++ HittableList.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "Hittable.h"
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
+
+class HittableList : public Hittable
+{
+public:
+	std::vector<shared_ptr<Hittable>> objects;
+
+	HittableList() {}
+	HittableList(shared_ptr<Hittable> object) 
+	{ 
+		add(object); 
+	}
+	void clear() { objects.clear(); }
+	void add(shared_ptr<Hittable> object)
+	{
+		objects.push_back(object);
+	}
+	virtual bool hit(const ray& r, double tMin, double tMax, hitRecord& rec) const override;
+};
+
+bool HittableList::hit(const ray& r, double tMin, double tMax, hitRecord& rec) const
+{
+	hitRecord tempRec;
+	bool hitAnything = false;
+	auto closestSoFar = tMax;
+
+	for (const auto& object : objects)
+	{
+		if (object->hit(r, tMin, closestSoFar, tempRec))
+		{
+			hitAnything = true;
+			closestSoFar = tempRec.t;
+			rec = tempRec;
+		}
+	}
+	return hitAnything;
+}
