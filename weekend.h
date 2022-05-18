24eadd2d9a53d4797723aa49eec5b3823d332e66
RoachCode <74122730+RoachCode@users.noreply.github.com>
2022-05-18 15:45:56 -0700

    Create weekend.h

---
+++ weekend.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <cmath>
+#include <limits>
+#include <memory>
+#include <cstdlib>
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degreesToRadians(double degrees)
+{
+	return degrees * pi / 180.0;
+}
+
+inline double randomDouble()
+{
+	return rand() / (RAND_MAX / 1.0);
+}
+
+inline double randomDouble(double min, double max)
+{
+	return min + (max - min) * randomDouble();
+}
+
+inline double clamp(double x, double min, double max)
+{
+	if (x < min) { return min; }
+	if (x > max) { return max; }
+	return x;
+}
+
+// Common Headers
+
+#include "Ray.h"
+#include "Vec3.h"
